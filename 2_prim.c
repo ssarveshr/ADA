d617e65a3ce88e0b917881bb394510f9e709ef4f
SAI SAVERSH R <ssr1032005@gmail.com>
2025-04-25 09:13:45 +0530

    changes

--- 2_prim.c
+++ 2_prim.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <limits.h>
 
-#define V_MAX 100 // Maximum number of vertices
+#define V_MAX 100
 
-// Function to find the vertex with the minimum key value, from the set of vertices not yet included in the MST
 int minKey(int key[], int mstSet[], int V) 
 {
     int min = INT_MAX, min_index;
-
     for (int v = 0; v < V; v++) 
     {
         if (mstSet[v] == 0 && key[v] < min) 
@@ -16,11 +14,9 @@ int minKey(int key[], int mstSet[], int V)
             min_index = v;
         }
     }
-
     return min_index;
 }
 
-// Function to print the constructed MST stored in parent[]
 void printMST(int parent[], int graph[V_MAX][V_MAX], int V) 
 {
     printf("Edge   Weight\n");
@@ -30,38 +26,28 @@ void printMST(int parent[], int graph[V_MAX][V_MAX], int V)
     }
 }
 
-// Function to construct and print MST for a graph represented using adjacency matrix
 void primMST(int graph[][V_MAX], int V) 
 {
-    int parent[V_MAX]; // Array to store constructed MST
-    int key[V_MAX];    // Key values used to pick minimum weight edge in cut
-    int mstSet[V_MAX]; // To represent set of vertices not yet included in MST
+    int parent[V_MAX];
+    int key[V_MAX];
+    int mstSet[V_MAX];
 
-    // Initialize all keys as INFINITE and mstSet[] as 0
     for (int i = 0; i < V; i++) 
     {
         key[i] = INT_MAX;
         mstSet[i] = 0;
     }
 
-    // Always include first vertex in MST. Make key 0 so that this vertex is picked as the first vertex
     key[0] = 0;
-    parent[0] = -1; // First node is always the root of MST
+    parent[0] = -1;
 
-    // The MST will have V vertices
     for (int count = 0; count < V - 1; count++) 
     {
-        // Pick the minimum key vertex from the set of vertices not yet included in MST
         int u = minKey(key, mstSet, V);
-
-        // Add the picked vertex to the MST set
         mstSet[u] = 1;
-
-        // Update key value and parent index of the adjacent vertices of the picked vertex
-        // Consider only those vertices which are not yet included in the MST
         for (int v = 0; v < V; v++) 
         {
-            if (graph[u][v] && mstSet[v] == 0 && graph[u][v] < key[v]) 
+            if (graph[u][v] == 1&& mstSet[v] == 0 && graph[u][v] < key[v]) 
             {
                 parent[v] = u;
                 key[v] = graph[u][v];
@@ -69,32 +55,23 @@ void primMST(int graph[][V_MAX], int V)
         }
     }
 
-    // Print the constructed MST
     printMST(parent, graph, V);
 }
 
 int main() 
 {
     int V, E;
-
     printf("Enter the number of vertices and edges: ");
     scanf("%d %d", &V, &E);
-
-    // Create the graph as an adjacency matrix
-    int graph[V_MAX][V_MAX] = {0}; // Initialize the graph with 0s
-
-    // Prompt the user to enter the source vertex, destination vertex, and weight for each edge
+    int graph[V_MAX][V_MAX] = {0};
     printf("Enter the source vertex, destination vertex, and weight for each edge:\n");
     for (int i = 0; i < E; i++) 
     {
         int source, dest, weight;
         scanf("%d %d %d", &source, &dest, &weight);
         graph[source][dest] = weight;
-        graph[dest][source] = weight; // Since the graph is undirected
+        graph[dest][source] = weight;
     }
-
-    // Print the MST using Prim's algorithm
     primMST(graph, V);
-
     return 0;
 }
---
+++ test/kruskal_test.c
@@ -0,0 +1,117 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX_EDGE 1000
+
+typedef struct edge
+{
+    int src,dest,weight;
+}edge;
+
+typedef struct subset
+{
+    int parent,rank;
+}subset;
+
+typedef struct Graph
+{
+    int E,V;
+    edge edges[MAX_EDGE];
+}Graph;
+
+Graph * createGrpah(int E,int V)
+{
+    Graph* g=(Graph*)malloc(sizeof(Graph));
+    g->E=E;
+    g->V=V;
+    return g;
+}
+
+int find(subset sub[],int i)
+{
+    if(sub[i].parent!=i)
+    {
+        sub[i].parent=find(sub,sub[i].parent);
+    }
+    return sub[i].parent;
+}
+
+void Union(subset sub[],int x,int y)
+{
+    x=find(sub,x);
+    y=find(sub,y);
+    if(sub[x].rank<sub[y].rank)
+    {
+        sub[x].parent=y;
+    }
+    else if(sub[x].rank>sub[y].rank)
+    {
+        sub[y].parent=x;
+    }
+    else
+    {
+        sub[y].parent=x;
+        sub[x].rank++;
+    }
+}
+
+int compare(const void* a,const void* b)
+{
+    edge* a_val=(edge*)a;
+    edge* b_val=(edge*)b;
+    return a_val->weight-b_val->weight;
+}
+
+void kruskalMST(Graph*g)
+{
+    edge mst[g->E];
+    int e=0,i=0;
+    qsort(g->edges,g->E,sizeof(edge),compare);
+
+    subset* sub=(subset*)malloc(g->V*sizeof(subset));
+
+    for(int v=0;v<g->V;++v)
+    {
+        sub[v].parent=v;
+        sub[v].rank=0;
+    }
+
+    while(e<g->V -1 && i<g->E)
+    {
+        edge next=g->edges[i++];
+        int x=find(sub,next.src);
+        int y=find(sub,next.dest);
+
+        if(x!=y)
+        {
+            mst[e++]=next;
+            Union(sub,x,y);
+        }
+    }
+
+    printf("min span trees\n");
+
+    for(int i=0;i<e;i++)
+    {
+        printf("(%d , %d) -> %d\n",mst[i].src,mst[i].dest,mst[i].weight);
+    }
+}
+
+
+int main()
+{
+    int V, E;
+    printf("Enter number of vertices and edges: ");
+    scanf("%d %d", &V, &E);
+
+    Graph* graph = createGrpah(V, E);
+
+    printf("Enter edges and their weights:\n");
+    for (int i = 0; i < E; ++i) 
+    {
+        scanf("%d %d %d", &graph->edges[i].src, &graph->edges[i].dest, &graph->edges[i].weight);
+    }
+
+    kruskalMST(graph);
+    return 0;
+}
+
